fa4619a9d3367f88b1603667fe7a725b3751f1f4
Heather Piverotto <heather@Huberts-MacBook-Pro.local>
2019-02-02 11:36:31 -0800

    complete promises

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,16 +6,31 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if(err) callback(err, null);
+    else {
+      var stringSplit = data.split('\n');
+      callback(null, stringSplit[0]);
+    }
+  })
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request(url, (err, res) => {
+    if(err) callback(err, null);
+    else {
+      callback(null, res.statusCode);
+    }
+  });
 };
 
-// Export these functions so we can test them and reuse them in later exercises
+
+
+
+// Export these functions so we can test them
+ //and reuse them in later exercises
 module.exports = {
   getStatusCode: getStatusCode,
   pluckFirstLineFromFile: pluckFirstLineFromFile
